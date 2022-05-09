67208dc7035ba5151a0baaf04e651a5865930300
Shun Ganas <sganas360@gmail.com>
2022-05-09 07:24:45 -0700

    Added some url and model testing

--- url_shortener_app/tests.py
+++ url_shortener_app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Url
 
-# Create your tests here.
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_testencodepage(self):
+        response = self.client.get("/encode")
+        self.assertEqual(response.status_code, 200)
+
+    def test_testdecodepage(self):
+        response = self.client.get("/decode")
+        self.assertEqual(response.status_code, 200)
+    
+class ModelTests(TestCase):
+    def test_model_url1(self):
+        url = Url.objects.create(original_url = "https://yello.co/resource/?_sft_resource_type=webinar", encoded_url = "http://short.est/1")
+        self.assertEqual(str(url.original_url) , "https://yello.co/resource/?_sft_resource_type=webinar")
+        self.assertEqual(str(url.encoded_url) , "http://short.est/1")
+        
+    def test_model_url2(self):
+        url = Url.objects.create(original_url = "https://github.com/sganas360/yello_url_shortener", encoded_url = "http://short.est/2")
+        self.assertEqual(str(url.original_url) , "https://github.com/sganas360/yello_url_shortener")
+        self.assertEqual(str(url.encoded_url) , "http://short.est/2")
